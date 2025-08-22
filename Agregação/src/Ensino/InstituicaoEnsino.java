359c2b806b8a3600ef7864f1c18d09ba703e5eb7
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-08-22 14:44:14 -0300

    Criando agregação

---
+++ Agregação/src/Ensino/InstituicaoEnsino.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ensino;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class InstituicaoEnsino{
+    private int id;
+    private String descricao;
+    private List <departamentos> Departamento = new ArrayList<departamentos>();
+
+
+}
